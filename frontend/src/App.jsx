8291ea06e804b7e19a1b302d74a1d709eee486b8
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2025-07-13 16:03:30 -0500

    base page, login, logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,104 @@
-import {Heading} from "./catalyst/heading.jsx";
+import {Avatar} from './catalyst/avatar'
+import {
+    Dropdown,
+    DropdownButton, DropdownDivider, DropdownHeading,
+    DropdownItem,
+    DropdownLabel,
+    DropdownMenu, DropdownSection,
+} from './catalyst/dropdown'
+import {Navbar, NavbarDivider, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer} from './catalyst/navbar'
+import {Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarLabel, SidebarSection} from './catalyst/sidebar'
+import {StackedLayout} from './catalyst/stacked-layout'
+import {
+    ArrowRightStartOnRectangleIcon,
+    UserIcon,
+} from '@heroicons/react/16/solid'
+import {Subheading} from "./catalyst/heading.jsx";
+import {Button} from "./catalyst/button.jsx";
 
-function App() {
+import pb from "./lib/pocketbase.js";
 
-  return (
-    <>
-      <Heading>Hello World</Heading>
-    </>
-  )
+const navItems = [
+    {label: 'Home', url: '/'},
+    {label: 'About', url: '/about'},
+]
+
+const App = ({children}) => {
+    const login = () => {
+        localStorage.setItem('redirect', 'https://gatherbuddy.bfrisco.io');
+        window.location.href = 'https://bricefrisco.com/login';
+    }
+
+    const logout = () => {
+        localStorage.removeItem('pocketbase_auth');
+        window.location.reload();
+    }
+
+    return (
+        <StackedLayout
+            navbar={
+                <Navbar>
+                    <NavbarLabel className="flex items-center ml-2">
+                        <Avatar src="/vite.svg" className="mr-2"/>
+                        <Subheading>GatherBuddy</Subheading>
+                    </NavbarLabel>
+                    <NavbarDivider className="max-lg:hidden"/>
+                    <NavbarSection className="max-lg:hidden">
+                        {navItems.map(({label, url}) => (
+                            <NavbarItem key={label} href={url}>
+                                {label}
+                            </NavbarItem>
+                        ))}
+                    </NavbarSection>
+                    <NavbarSpacer/>
+                    <NavbarSection>
+                        {pb.authStore.isValid ? (
+                            <Dropdown>
+                                <DropdownButton as={NavbarItem}>
+                                    <UserIcon/>
+                                </DropdownButton>
+                                <DropdownMenu className="min-w-64" anchor="bottom end">
+                                    <DropdownSection>
+                                        <DropdownHeading>
+                                            {pb.authStore.record.email}
+                                        </DropdownHeading>
+                                    </DropdownSection>
+                                    <DropdownDivider />
+                                    <DropdownItem onClick={() => logout()}>
+                                        <ArrowRightStartOnRectangleIcon/>
+                                        <DropdownLabel>Sign out</DropdownLabel>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        ) : (
+                            <Button onClick={() => login()}>Sign in</Button>
+                        )}
+                    </NavbarSection>
+                </Navbar>
+            }
+            sidebar={
+                <Sidebar>
+                    <SidebarHeader>
+                        <SidebarLabel className="flex items-center mt-1">
+                            <Avatar src="/vite.svg" className="mr-2"/>
+                            <Subheading>GatherBuddy</Subheading>
+                        </SidebarLabel>
+                    </SidebarHeader>
+                    <SidebarBody>
+                        <SidebarSection>
+                            {navItems.map(({label, url}) => (
+                                <SidebarItem key={label} href={url}>
+                                    {label}
+                                </SidebarItem>
+                            ))}
+                        </SidebarSection>
+                    </SidebarBody>
+                </Sidebar>
+            }
+        >
+            {children}
+        </StackedLayout>
+    )
 }
 
-export default App
+export default App;
